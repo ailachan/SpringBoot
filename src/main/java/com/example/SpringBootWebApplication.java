f4309c328b07fe090a1ec8618d27fdba3aa9b9ae
aila <32830121@qq.com>
2023-03-13 16:04:52 +0800

    web project push modify2 commit

--- src/main/java/com/example/SpringBootWebApplication.java
+++ src/main/java/com/example/SpringBootWebApplication.java
@@ -2,10 +2,16 @@ package com.example;
 
 import com.example.controller.interceptor.ProjectInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.lang.reflect.Field;
+import java.util.Map;
 
 @SpringBootApplication
 public class SpringBootWebApplication implements WebMvcConfigurer {
@@ -13,8 +19,20 @@ public class SpringBootWebApplication implements WebMvcConfigurer {
     @Autowired
     private ProjectInterceptor projectInterceptor;
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootWebApplication.class, args);
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootWebApplication.class, args);
+
+        ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
+
+        //beanFactory接口提供getBean,控制反转由实现类提供
+        Field singletonObjects = DefaultSingletonBeanRegistry.class.getDeclaredField("singletonObjects");
+
+        singletonObjects.setAccessible(true);
+
+        Map<String,Object> o = (Map<String, Object>) singletonObjects.get(beanFactory);
+
+        o.entrySet().stream().filter(x -> x.getKey().startsWith("controller")).forEach(System.out::println);
+
     }
 
     //添加一个拦截器
